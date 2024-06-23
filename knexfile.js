52bfe8306eb62216e666cfb8dacd54dc43ca529e
Carolina Quiles <145223793+carolinaquiles@users.noreply.github.com>
2024-06-23 11:34:52 -0400

    Summary
    added Migrations with Knex

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,8 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
@@ -10,5 +12,8 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
   },
 };
--- src/db/fixtures/connection.js
+++ src/db/fixtures/connection.js
@@ -1,5 +1,3 @@
 const env = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[env];
 const knex = require("knex")(config);
-
-module.exports = knex;
